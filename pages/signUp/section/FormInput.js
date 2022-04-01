139d6b7f9940e1a4e36301613b6f62ab244b5f74
unknown <you@example.com>
2022-04-01 14:11:14 +0530

    0.0.1

--- pages/signUp/section/FormInput.js
+++ pages/signUp/section/FormInput.js
@@ -3,6 +3,7 @@ import axios from "axios";
 import { useRouter } from "next/router";
 import { notification } from "antd";
 import { SmileOutlined } from "@ant-design/icons";
+import { Modal, Button } from "antd";
 
 // import baseURL from "../../../Helpers/Globals";
 const baseURL = "http://localhost:1000";
@@ -19,6 +20,10 @@ export default function FormInput() {
 
   const [users, setUsers] = useState([]);
   const [errorMsg, setErrorMsg] = useState("Error Retriving data");
+
+  const [selectedUserData, setSelectedUserData] = useState(null);
+  const [triggerUseEffect, setuseEffect] = useState(false);
+  const [isModalVisible, setIsModalVisible] = useState(false);
 
   // const HandleFullNameChange = (e) => {
   //   setFullName(e.target.value);
@@ -47,14 +52,16 @@ export default function FormInput() {
     axios // .get("https://jsonplaceholder.typicode.com/users") // .get("http://localhost:1000/api/get/users") // .get("/api/get/users")
       .get(`${baseURL}/api/get/all-users`)
       .then((res) => {
-        const data = res.data;
-        setUsers(data);
+        const GetUsersData = res.data.data;
+        console.log(res.data.data);
+        // setUsers(...users, data);
+        setUsers(GetUsersData);
         console.log("Data has been received", data);
       })
       .catch((err) => {
         console.log("Error in getUsers", err);
       });
-  }, []);
+  }, [triggerUseEffect]);
 
   const deleteUser = (_id) => {
     const newUsersAfterDeletion = users.filter((newUser) => {
@@ -84,7 +91,6 @@ export default function FormInput() {
       // phoneNo: phoneNo,
       // roomNo: roomNo,
     };
-    // console.log({ fullName, regNo, dept, email });
     axios
       .post(`${baseURL}/api/post/add-user`, payload)
       .then((res) => {
@@ -95,29 +101,45 @@ export default function FormInput() {
           description: "Saved Successfully in Database",
           placement: "topRight",
         });
+        setuseEffect(!triggerUseEffect);
       })
       .catch((err) => {
         console.log("Internal Server Error - handleSubmit", err);
+        notification.error({
+          message: "Error",
+          description: "Error in HandleSubmit - Axios POST",
+          placement: "topRight",
+        });
       });
 
     setFullName("");
     setRegNo("");
     setDept("");
     setEmail("");
+  };
 
-    // axios
-    //   .get(`${baseURL}/api/get/user/:_id`)
-    //   .then((res) => {
-    //     console.log(res);
-    //   })
-    //   .catch((err) => {
-    //     console.log(err);
-    //   });
+  const updateHandler = (e) => {
+    e.preventDefault();
+
+    axios
+      .put(`${baseURL}/api/update/all-users`, {
+        id: selectedUserData._id,
+        updatedUserData: selectedUserData,
+      })
+      .then((res) => {
+        console.log(res);
+        setIsModalVisible(false);
+        setuseEffect(!triggerUseEffect);
+      })
+      .catch((err) => {
+        console.log("Error in updateHandler", err);
+      });
+    console.log(selectedUserData);
   };
 
   return (
     <div>
-      <form onSubmit={handleSubmit} className="p-24">
+      <form onSubmit={() => handleSubmit} className="p-24">
         <button onClick={openNotify}>Notify</button>
         <div>
           <label>Full Name</label>
@@ -202,7 +224,14 @@ export default function FormInput() {
                   <td className="w-56 text-center">{user.email}</td>
                   <td className="w-56 text-center">{user.createdAt}</td>
                   <td className="w-40 text-center">
-                    <button onClick={() => deleteUser(user._id)}>Update</button>
+                    <button
+                      onClick={() => {
+                        setSelectedUserData(user);
+                        setIsModalVisible(true);
+                      }}
+                    >
+                      Update
+                    </button>
                   </td>
                   <td className="w-40 text-center">
                     <button onClick={() => deleteUser(user._id)}>Delete</button>
@@ -215,9 +244,113 @@ export default function FormInput() {
           </tbody>
         </table>
       </div>
+
+      <Modal
+        title="Update Student"
+        visible={isModalVisible}
+        onCancel={() => setIsModalVisible(false)}
+        footer={[
+          <Button
+            key="update"
+            type="primary"
+            // loading={loading}
+            onClick={() => updateHandler}
+          >
+            Update
+          </Button>,
+        ]}
+      >
+        {console.log(selectedUserData)}
+        <section>
+          <form onSubmit={updateHandler}>
+            <label>Full Name</label>
+            <input
+              type={"text"}
+              placeholder={"Ex: Akbar Sha S"}
+              onChange={(e) => {
+                setSelectedUserData({
+                  ...selectedUserData,
+                  fullName: e.target.value,
+                });
+              }}
+              value={selectedUserData !== null ? selectedUserData.fullName : ""}
+            />
+            <button>Update</button>
+          </form>
+        </section>
+      </Modal>
     </div>
   );
 }
+
+// import { Modal, Button } from 'antd';
+
+// class App extends React.Component {
+//   state = {
+//     loading: false,
+//     visible: false,
+//   };
+
+//   showModal = () => {
+//     this.setState({
+//       visible: true,
+//     });
+//   };
+
+//   handleOk = () => {
+//     this.setState({ loading: true });
+//     setTimeout(() => {
+//       this.setState({ loading: false, visible: false });
+//     }, 3000);
+//   };
+
+//   handleCancel = () => {
+//     this.setState({ visible: false });
+//   };
+
+//   render() {
+//     const { visible, loading } = this.state;
+//     return (
+//       <>
+//         <Button type="primary" onClick={this.showModal}>
+//           Open Modal with customized footer
+//         </Button>
+//         <Modal
+//           visible={visible}
+//           title="Title"
+//           onOk={this.handleOk}
+//           onCancel={this.handleCancel}
+//           footer={[
+//             <Button key="back" onClick={this.handleCancel}>
+//               Return
+//             </Button>,
+//             <Button key="submit" type="primary" loading={loading} onClick={this.handleOk}>
+//               Submit
+//             </Button>,
+//             <Button
+//               key="link"
+//               href="https://google.com"
+//               type="primary"
+//               loading={loading}
+//               onClick={this.handleOk}
+//             >
+//               Search on Google
+//             </Button>,
+//           ]}
+//         >
+//           <p>Some contents...</p>
+//           <p>Some contents...</p>
+//           <p>Some contents...</p>
+//           <p>Some contents...</p>
+//           <p>Some contents...</p>
+//         </Modal>
+//       </>
+//     );
+//   }
+// }
+
+// ReactDOM.render(<App />, mountNode);
+
 // const inputs = [
 //   {
 //     id: 1,
