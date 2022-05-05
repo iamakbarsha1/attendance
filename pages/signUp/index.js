6fae9ba7d35b4ec038efb43ca4728a3ce0237eaf
unknown <you@example.com>
2022-05-05 14:44:25 +0530

    Phase 1

--- pages/signUp/index.js
+++ pages/signUp/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import axios from "axios";
 import { useState } from "react";
-import FormInput from "./section/FormInput";
+// import FormInput from "./section/FormInput";
 
 function Create() {
   // const [formData, setFormData] = useState({
